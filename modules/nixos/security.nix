c0187e45b926745ac0886cbc2c4f79d191bc104a
alexrf45 <fonalex45@gmail.com>
2026-04-12 21:55:40 -0400

    fix: source _1password-gui-beta from pkgs-unstable (8.12.0 vs 8.11.18 in stable)

--- modules/nixos/security.nix
+++ modules/nixos/security.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 {
   # -----------------------------------------------------------------------
   # SSH daemon
@@ -36,7 +36,7 @@
   programs._1password-gui = {
     enable = true;
     polkitPolicyOwners = [ "fr3d" ];
-    package = pkgs._1password-gui-beta;
+    package = pkgs-unstable._1password-gui-beta;
   };
 
   # -----------------------------------------------------------------------
