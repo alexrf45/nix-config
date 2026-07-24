200256e11f98844ca0e829952560637041bb2213
alexrf45 <fonalex45@gmail.com>
2026-07-23 22:28:18 -0400

    new theme

--- modules/home-manager/terminal.nix
+++ modules/home-manager/terminal.nix
@@ -1,6 +1,6 @@
 {...}: {
   # -----------------------------------------------------------------------
-  # kitty — GPU terminal, flow.nvim moon-umbra palette (pink fluo accent)
+  # kitty — GPU terminal, moonfly palette (github.com/bluz71/vim-moonfly-colors)
   # -----------------------------------------------------------------------
   programs.kitty = {
     enable = true;
@@ -18,35 +18,42 @@
       confirm_os_window_close = 0;
       enable_audio_bell = "no";
 
-      # flow.nvim moon-umbra (dark, pink fluo accent)
-      background = "#1f282e";
-      foreground = "#94aab8";
+      # moonfly (dark)
+      background = "#080808";
+      foreground = "#bdbdbd";
 
-      cursor = "#ff007b";
-      cursor_text_color = "#0d0d0d";
+      cursor = "#9e9e9e";
+      url_color = "#79dac8";
 
-      selection_background = "#0d0d0d";
-      selection_foreground = "#ff007b";
+      selection_background = "#b2ceee";
+      selection_foreground = "#080808";
 
       # normal
-      color0 = "#0d0d0d"; # black
-      color1 = "#862d34"; # red
-      color2 = "#34862d"; # green
-      color3 = "#86862d"; # yellow
-      color4 = "#2d6186"; # blue
-      color5 = "#592d86"; # magenta
-      color6 = "#2d8670"; # cyan
-      color7 = "#f2f2f2"; # white
+      color0 = "#323437"; # black
+      color1 = "#ff5d5d"; # red
+      color2 = "#8cc85f"; # green
+      color3 = "#e3c78a"; # yellow
+      color4 = "#80a0ff"; # blue
+      color5 = "#cf87e8"; # magenta
+      color6 = "#79dac8"; # cyan
+      color7 = "#c6c6c6"; # white
 
       # bright
-      color8 = "#3b4d59"; # bright black
-      color9 = "#862d34"; # bright red
-      color10 = "#34862d"; # bright green
-      color11 = "#86862d"; # bright yellow
-      color12 = "#2d6186"; # bright blue
-      color13 = "#592d86"; # bright magenta
-      color14 = "#2d8670"; # bright cyan
-      color15 = "#f2f2f2"; # bright white
+      color8 = "#949494"; # bright black
+      color9 = "#ff5189"; # bright red
+      color10 = "#36c692"; # bright green
+      color11 = "#c6c684"; # bright yellow
+      color12 = "#74b2ff"; # bright blue
+      color13 = "#ae81ff"; # bright magenta
+      color14 = "#85dc85"; # bright cyan
+      color15 = "#e4e4e4"; # bright white
+
+      active_tab_foreground = "#080808";
+      active_tab_background = "#80a0ff";
+      inactive_tab_foreground = "#b2b2b2";
+      inactive_tab_background = "#323437";
+      active_border_color = "#80a0ff";
+      inactive_border_color = "#323437";
     };
   };
 }
